14561d0ae00d1810b174dd3a9397959faf9e39cc
Darus <drsshk@yahoo.com.my>
2025-07-14 09:49:28 +0800

    removed CLIENT_CLASS - invalid for Django's built-in Redis cache backend

--- docuhub/settings.py
+++ docuhub/settings.py
@@ -136,7 +136,6 @@ else:
         'default': {
             'BACKEND': 'django.core.cache.backends.redis.RedisCache',
             'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
-            'OPTIONS': {'CLIENT_CLASS': 'django_redis.client.DefaultClient'},
             'KEY_PREFIX': 'docuhub',
             'TIMEOUT': 300,
         }
